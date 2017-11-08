f525a40736c810558253b21cfb32bcebe60cd419
sallybananas <saljgiani@gmail.com>
2017-11-08 11:02:45 -0700

    tried out type.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,11 +9,11 @@
 // $('.nav-tabs a[href="#how"]').tab('show')
 
 // $('.nav-tabs a[href="#how"]').tab('show')
-$('select').change(function() {
-    if ($(this).children('option:first-child').is(':selected')) {
-      $(this).addClass('placeholder');
-    } else {
-     $(this).removeClass('placeholder');
-    }
-   });
 
+$('select').change(function () {
+  if ($(this).children('option:first-child').is(':selected')) {
+    $(this).addClass('placeholder');
+  } else {
+    $(this).removeClass('placeholder');
+  }
+});
