815f8255c9ec1f8ca1aed31b5cc1514c42fbc1ec
Mehathab4eva <mehathab4eva@gmail.com>
2017-11-09 19:06:20 -0700

    lets check

---
+++ assets/js/youtube.js
@@ -0,0 +1,5 @@
+//https://www.youtube.com/results?search_query=grit
+var youtubeURL = "https://www.youtube.com/results?search_query=";
+$("#youGrit").on("click",fuction(){
+  console.log("its working")
+});
