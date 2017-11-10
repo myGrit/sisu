b000211650762d794e80028734ea7035f641e28e
mpdrsn <{mpdrsn}@users.noreply.github.com>
2017-11-09 20:37:24 -0700

    Formatting and pre-delta

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,21 @@
-// // Select all tabs
-// $('#fixtab').click(function(){
-//     console.log("how button clicked");
-//     $(this).tab('show');
-// })
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDNJ4bHrhTKKnQu59EcFaAa7YzzIZMU5HI",
+	authDomain: "mygrit-01.firebaseapp.com",
+	databaseURL: "https://mygrit-01.firebaseio.com",
+	projectId: "mygrit-01",
+	storageBucket: "mygrit-01.appspot.com",
+	messagingSenderId: "20968145830"
+};
+firebase.initializeApp(config);
 
-// // Select tab by name
 
-// $('.nav-tabs a[href="#how"]').tab('show')
-
-// $('.nav-tabs a[href="#how"]').tab('show')
+// Identify active tab
 $('select').change(function() {
-    if ($(this).children('option:first-child').is(':selected')) {
-      $(this).addClass('placeholder');
-    } else {
-     $(this).removeClass('placeholder');
-    }
-   });
+	if ($(this).children('option:first-child').is(':selected')) {
+	  $(this).addClass('placeholder');
+	} else {
+	 $(this).removeClass('placeholder');
+	}
+});
 
